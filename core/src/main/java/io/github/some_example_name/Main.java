3c3e9d71c41a6d8664adb44efc0962581735bdf2
italonrg <italonrg500@gmail.com>
2024-10-09 10:26:25 -0300

    Adicionando o movimento a nave

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -5,24 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture image,tNave;// isso cria a imagem que vai ter que ser convertida num sptite;
+    private Sprite nave;// isso cria o spite;
+    private float posX , posY;// estas variaveis servem para controlar o movimento da nave;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("bg.png");//fundo
+        tNave = new Texture("spaceship.png");//nave
+        nave= new Sprite(tNave);
+        posX = 0;
+        posY = 0;
     }
 
     @Override
     public void render() {
+        this.moveNave();
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         batch.draw(image, 140, 210);
+        batch.draw(nave ,posX,posY);
         batch.end();
     }
 
@@ -30,5 +41,22 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+        tNave.dispose();
+    }
+
+    private void moveNave(){
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            posX += 10;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            posX -= 10;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            posY += 10;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            posY -= 10;
+        }
     }
 }
